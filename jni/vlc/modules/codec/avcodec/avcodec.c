d502e1c97e6d7392f7096a5bbe682768a1f2d64f
tewilove <tewilove@gmail.com>
2011-05-17 21:37:29 +0800

    Apple HTTP Streaming workaround

--- jni/vlc/modules/codec/avcodec/avcodec.c
+++ jni/vlc/modules/codec/avcodec/avcodec.c
@@ -400,8 +400,7 @@ int ffmpeg_OpenCodec( decoder_t *p_dec )
     {
         if( p_sys->i_codec_id == CODEC_ID_VC1 ||
             p_sys->i_codec_id == CODEC_ID_VORBIS ||
-            p_sys->i_codec_id == CODEC_ID_THEORA ||
-            p_sys->i_codec_id == CODEC_ID_AAC )
+            p_sys->i_codec_id == CODEC_ID_THEORA )
         {
             msg_Warn( p_dec, "waiting for extra data for codec %s",
                       p_sys->psz_namecodec );
