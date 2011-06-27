6d0c3b7870755c0b1e627ab7b486b26e6e8bd5d3
tewilove <tewilove@gmail.com>
2011-06-27 16:07:20 +0800

    specify build target from environment

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,23 +1,44 @@
 # armeabi/armeabi-v7a
-APP_ABI := armeabi-v7a
-# default depends on AndroidManifest.xml
-APP_OPTIM := release
-# default is -O2
-OPT_CFLAGS := -O3 -mlong-calls -fstrict-aliasing -fprefetch-loop-arrays -ffast-math
-ifeq ($(APP_ABI),armeabi-v7a)
-# most common case
-OPT_CFLAGS += -mfpu=neon -mtune=cortex-a8 -ftree-vectorize -mvectorize-with-neon-quad
-BUILD_WITH_NEON := 1
-# tegra 2 without neon
-#OPT_CFLAGS += -mfpu=vfpv3 -mtune=cortex-a9 -ftree-vectorize
-#BUILD_WITH_NEON := 0
-else
-OPT_CFLAGS += -march=armv6j -mtune=arm1136j-s -msoft-float
-BUILD_WITH_NEON := 0
+
+_ABI ?= $(ABI)
+ABI := $(_ABI)
+_FPU ?= $(FPU)
+FPU := $(_FPU)
+_TUNE ?= $(TUNE)
+TUNE := $(_TUNE)
+
+ifeq ($(ABI),)
+    ABI := armeabi-v7a
 endif
+ifeq ($(FPU),)
+    FPU := neon
+endif
+ifeq ($(TUNE),)
+    TUNE := cortex-a8
+endif
+
+APP_ABI := $(ABI)
+APP_OPTIM := release
+
+OPT_CFLAGS := -O3 -mlong-calls -fstrict-aliasing -fprefetch-loop-arrays -ffast-math
+
+ifeq ($(APP_ABI),armeabi-v7a)
+    ifeq ($(FPU),neon)
+        OPT_CFLAGS += -mfpu=neon -mtune=$(TUNE) -ftree-vectorize -mvectorize-with-neon-quad
+        BUILD_WITH_NEON := 1
+    else
+        OPT_CFLAGS += -mfpu=$(FPU) -mtune=$(TUNE) -ftree-vectorize
+        BUILD_WITH_NEON := 0
+    endif
+else
+    OPT_CFLAGS += -march=armv6j -mtune=arm1136j-s -msoft-float
+    BUILD_WITH_NEON := 0
+endif
+
 OPT_CPPFLAGS := $(OPT_CLFAGS)
-# override default
+
 APP_CFLAGS := $(APP_CFLAGS) $(OPT_CFLAGS)
 APP_CPPFLAGS := $(APP_CPPFLAGS) $(OPT_CPPFLAGS) 
-# stl
+
 APP_STL := stlport_static
+
